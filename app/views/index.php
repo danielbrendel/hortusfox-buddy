43bb5948cada04d3737006329d36757686eaac68
Daniel Brendel <dbrendel1988@gmail.com>
2025-03-22 02:25:05 +0100

    Update instructions & add sponsoring section

--- app/views/index.php
+++ app/views/index.php
@@ -29,15 +29,26 @@
 
         <li>When the bot has joined your server, do the following setup commands</strong>
             <ol>
-                <li><strong>/admin set apikey (your-api-key)</strong> to set your Pl@ntNet API key</li>
-                <li><strong>/admin set recchan (channel name)</strong> to set the plant recognition channel</li>
+                <li><strong>/hfbud set apikey (your-api-key)</strong> to set your Pl@ntNet API key</li>
+                <li><strong>/hfbud set recchan (channel name)</strong> to set the plant recognition channel</li>
+                <li><strong>/hfbud set gamechan (channel name)</strong> to set the plant guessing game channel</li>
             </ol>
         </li>
 
-        <li>You can run the following general commands</strong>
+        <li>You can then run the following additional admin commands</strong>
             <ol>
-                <li><strong>/admin stats</strong> to see the current bot statistics for your server</li>
-                <li><strong>/info</strong> can be used by anyone to print general information about the bot</li>
+                <li><strong>/hfbud game start</strong> Start the plant guessing game</li>
+                <li><strong>/hfbud game stop</strong> Stop the plant guessing game</li>
+                <li><strong>/hfbud game leaderboard</strong> Show leaderboard and end current session. Scores will be reset.</li>
+                <li><strong>/hfbud stats</strong> to see the current bot statistics for your server</li>
+            </ol>
+        </li>
+
+        <li>These commands are available to anyone</strong>
+            <ol>
+                <li><strong>/guess (plant name)</strong> Guess the plant name of a currently appeared plant</li>
+                <li><strong>/plantscore</strong> to see a members current plant guessing game score</li>
+                <li><strong>/hortusbuddy</strong> can be used by anyone to print general information about the bot</li>
             </ol>
         </li>
         
@@ -45,3 +56,19 @@
         <li>Join the <a href="{{ env('LINK_DISCORD') }}">HortusFox discord server</a> for support!</li>
     </ol>
 </div>
+
+@if (env('APP_ENABLESPONSORING'))
+<div class="sponsoring">
+    <h2><i class="fas fa-heart"></i> Your support is greatly appreciated</h2>
+
+    <div>
+        <div class="sponsoring-item">
+            <iframe src="https://github.com/sponsors/danielbrendel/button" title="Sponsor danielbrendel" height="32" width="114" style="border: 0; border-radius: 6px;"></iframe>
+        </div>
+
+        <div class="sponsoring-item">
+            <a href='https://ko-fi.com/C0C7V2ESD' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+        </div>
+    </div>
+</div>
+@endif
